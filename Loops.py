06814a0edc9dd983e639801935397186df2b7c39
fatih arslan <fatih.316.arslan@gmail.com>
2024-09-24 23:54:33 +0300

    update
    List, Tupple and Dictionary.

--- Loops.py
+++ Loops.py
@@ -21,16 +21,38 @@ else:
     print('I appended it!!')
     print('New list {}'.format(list_1))
 
+# for loop
 
+names = ["Jack William","Nick Cage","Andrew Blake","Bob Marly","Alica Clark","Kate Snow","Sonya Zack"]
+name_no = 0
 
+#for name in names:
+#    print(name)
 
+for n in names:
+    name_no +=1
+    name, surname = n.split()[0], n.split()[1]
+    print('{0}. Name {1} surname {2}.'.format(name_no,name,surname))
 
+# Tupple
 
+tup1 = (1,2,3,4,5)
+for no in tup1:
+    print(no)
 
+list1 = [[1,2,3],[4,5,6]]
+for x,y,z in list1:
+    print(y)
 
+#Dictionary
 
+users = {
+    'name' : 'Kate',
+    'surname' : 'Bishop'
+}
 
-
+for n,s in users.items():
+    print("Key:  {} \tvalue: {}".format(n,s))
 
 
 
